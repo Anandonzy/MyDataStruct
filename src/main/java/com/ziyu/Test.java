a19450f26cb8a11701011b8c9975523e9396fc81
wangziyu1 <wangziyu1@yiche.com>
2020-06-16 17:41:26 +0800

    第一次提交git

---
+++ src/main/java/com/ziyu/Test.java
@@ -0,0 +1,12 @@
+package com.ziyu;
+
+/**
+ * @author wangziyu1
+ * @date 2020-06-16 17:39
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("first add to git");
+    }
+}
