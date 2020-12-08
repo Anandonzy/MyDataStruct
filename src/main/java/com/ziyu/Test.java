59e08177d4de2818b8d930a37871985a753a1837
wangziyu1 <wangziyu1@yiche.com>
2020-12-08 11:50:14 +0800

    git test

--- src/main/java/com/ziyu/Test.java
+++ src/main/java/com/ziyu/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("first add to git");
         System.out.println("second add to git");
+        System.out.println("therd add to git");
     }
 }
